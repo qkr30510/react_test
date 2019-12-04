7076e4642b342e5c642eef0040f21c98dbdfefab
juhee <qkr30510@naver.com>
2019-12-04 14:24:34 +0900

    [todo-list] 숙제까지 완료

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import TodoListTemplate from './components/TodoListTemplate';
 import Form from './components/Form';
 import TodoItemList from './components/TodoItemList';
+import Palette from './components/Palette';
 
+
+const colors = ['#343a40', '#f03e3e', '#12b886', '#228ae6'];
 
 class App extends Component {
 
@@ -12,10 +15,12 @@ class App extends Component {
     input: '',
     todos: [
       { id: 0, text: ' 리액트 소개', checked: false },
-      { id: 1, text: ' 리액트 소개', checked: true },
-      { id: 2, text: ' 리액트 소개', checked: false }
-    ]
+      { id: 1, text: 'JSX 사용해보기', checked: true },
+      { id: 2, text: '라이프 사이클 이해하기', checked: false },
+    ],
+    color: '#343a40'
   }
+
   handleChange = (e) => {
     this.setState({
       input: e.target.value // input 의 다음 바뀔 값
@@ -23,14 +28,15 @@ class App extends Component {
   }
 
   handleCreate = () => {
-    const { input, todos } = this.state;
+    const { input, todos, color } = this.state;
     this.setState({
       input: '', // 인풋 비우고
       // concat 을 사용하여 배열에 추가
       todos: todos.concat({
         id: this.id++,
         text: input,
-        checked: false
+        checked: false,
+        color
       })
     });
   }
@@ -41,16 +47,16 @@ class App extends Component {
       this.handleCreate();
     }
   }
-  
+
   handleToggle = (id) => {
     const { todos } = this.state;
-
+    
     // 파라미터로 받은 id 를 가지고 몇번째 아이템인지 찾습니다.
     const index = todos.findIndex(todo => todo.id === id);
     const selected = todos[index]; // 선택한 객체
 
     const nextTodos = [...todos]; // 배열을 복사
-
+    
     // 기존의 값들을 복사하고, checked 값을 덮어쓰기
     nextTodos[index] = { 
       ...selected, 
@@ -62,13 +68,28 @@ class App extends Component {
     });
   }
 
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    });
+  }
+
+  handleSelectColor = (color) => {
+    this.setState({
+      color
+    })
+  }
+
   render() {
-    const { input, todos } = this.state;
+    const { input, todos, color } = this.state;
     const {
       handleChange,
       handleCreate,
       handleKeyPress,
-      handleToggle
+      handleToggle,
+      handleRemove,
+      handleSelectColor
     } = this;
 
     return (
@@ -78,9 +99,13 @@ class App extends Component {
           onKeyPress={handleKeyPress}
           onChange={handleChange}
           onCreate={handleCreate}
+          color={color}
         />
-      )}>
-        <TodoItemList todos={todos} onToggle={handleToggle}/>
+      )}
+        palette={(
+          <Palette colors={colors} selected={color} onSelect={handleSelectColor}/>
+        )}>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
       </TodoListTemplate>
     );
   }
