b6abfcddc8efaa3f27684570e1c2eff8e5a9f124
juhee <qkr30510@naver.com>
2020-03-27 22:38:58 +0900

    벨로퍼트 스터디 ~라이프사이클 메서드까지

--- dutch-project/src/App.js
+++ dutch-project/src/App.js
@@ -1,15 +1,31 @@
 import React, {Component} from "react";
-import ScrollBox from "./ScrollBox";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from './ErrorBoundary';
+
+//램덤 색상을 생성합니다. 
+function getRandomColor (){
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state ={
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState ({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
       <div>
-        <ScrollBox ref={ref => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color}/>
+        </ErrorBoundary>
+      </div>      
     );
   }
 }
