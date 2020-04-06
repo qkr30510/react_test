b6eea8df00edf61ae0f69312ee9e9fee9148161c
juhee <qkr30510@naver.com>
2020-04-06 18:38:00 +0900

    컴포넌트 성능 최적화

--- todo-project/src/App.js
+++ todo-project/src/App.js
@@ -1,10 +1,41 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useReducer, useRef, useCallback } from 'react';
 import Message from './Message';
 import TodoInsert from './TodoInsert';
 import TodoList from './TodoList';
 
+function creatBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT': // 새로 추가
+      //{type: 'INSERT', todo: {id: 1, text: 'todo', checked:false}}
+      return todos.concat(action.todo);
+    case 'REMOVE': //제거
+      //{type:'REMOVE', id:1}
+      return todos.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE': //토글
+      //{type: 'TOGGLE'}
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
 const App = () => {
-  const [todos, setTodos] = useState([
+  const [todos, dispatch] = useReducer(todoReducer, undefined, creatBulkTodos);
+  /*[
     {
       id: 1,
       text: '리액트의 기초 알아보기',
@@ -20,46 +51,34 @@ const App = () => {
       text: '일정 관리 앱 만들어 보기',
       checked: false,
     },
-  ]);
-  
+  ]);*/
+
   //고윳값으로 사용될 id
   // ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
-  const onInsert = useCallback(
-    text => {
-      const todo ={
-        id : nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.cuttent += 1; // nextId 1씩 더하기 
-    },
-    [todos],
-  );
-  const onRemove = useCallback(
-    id => {
-      setTodos(todos.filter(todo => todo.id !== id));
-    },
-    [todos],
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: 'INSERT', todo });
+    nextId.cuttent += 1; // nextId 1씩 더하기
+  }, []);
 
-  const onToggle = useCallback (
-    id => {
-      setTodos(
-        todos.map(todo =>
-          todo.id === id ? { ...todo, checked: !todo.checked} : todo,
-          ),
-      );
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
+
+  const onToggle = useCallback((id) => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
 
   return (
     <Message>
-      <TodoInsert onInsert={onInsert}/>
-      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </Message>
   );
 };
